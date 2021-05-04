3a045290ce042ad5e6d07e2125c4a8eeeb777907
Diesel <matheusdiesel01@gmail.com>
2021-05-04 11:17:52 -0300

    Added styling to Home/About (I should change these names...)

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./styles/home.css"
 
 const About = () => { 
     return (
-        <div>
-             <h1>
-                Bem-vindxs ao curso de Lógica!
-            </h1>
+        <>
+            <div className = "main-div">
+                <div className = "img-div">
+                    <img className = "main-img" src = "./imgs/home.svg" alt = "Lógica"></img>
+                </div>
+                    
+                <div className = "txt-div"> 
+                    <h1 className = "main-title">
+                        Bem-vindxs ao curso de Lógica e Argumentação!
+                    </h1>
 
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Felis imperdiet proin fermentum leo. Laoreet non curabitur gravida arcu ac tortor dignissim convallis. 
-                Sed turpis tincidunt id aliquet risus. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla.
-            </p>
-        </div>
+                    <div>
+                        <p className = "home-txt">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  
+                        </p>
+
+                        <p className = "home-txt">
+                            Odio ut sem nulla pharetra diam. Suspendisse in est ante in.
+                            WJlskfjdsf sfjlsdfj dfkgjflg fkgjsdlfk dfjs slfjsgf. 
+                            sdklgjlsgj, dsfklj fssgs dfkfk dsfosdgdgg
+                        </p>
+                    </div>
+
+                    <div className = "link-pill">
+                        <Link to = "/linha" className = "home-link">
+                            Clique aqui para começar
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+            <footer>
+                TODO: foofer
+            </footer>
+        </>
     )
 }
 
