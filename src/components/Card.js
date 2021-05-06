6cd30db7c8e6fdb0ca7658d0721fea81881c8822
Diesel <matheusdiesel01@gmail.com>
2021-05-06 14:27:11 -0300

    Added CSS to Linha and Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,16 @@
 import React from "react";
+import "./styles/card.css"
 
 const Card = (props) => {
 
     return (
-        <div style = {{
-            display: "grid",
-            background: "#FCFCFC",
-            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-            borderRadius: "49px",
-            width: "110%", 
-            gridTemplateRows: "auto auto auto", 
-            justifyContent: "center"
-        }}>
+        <div className = "card-div">
 
-            <h2 style = {{margin: "auto"}}>{props.title}</h2>
+            <h2 className = "card-h2">{props.title}</h2>
+            
+            <img src = {`./imgs/${props.img}.svg`} alt = {`${props.title}`}/>
 
-                <img src = {`./imgs/${props.img}.svg`} alt = {`${props.title}`}/>
-
-            <p style = {{
-                margin: "auto", 
-                paddingBottom: "5%"
-            }}>
+            <p className = "card-txt">
                 {props.txt}
             </p>
 
--- src/components/Linha.js
+++ src/components/Linha.js
@@ -7,24 +7,34 @@ const Linha = () => {
     return (
         <main>
             <Nav></Nav>
-
+            
              <div style = {{
                     display: "grid", 
                     gridTemplateColumns: "auto auto auto", 
-                    alignContent: "center", justifyContent: "center", 
-                    placeContent: "space-evenly"
+                    justifyContent: "space-evenly", 
+                    alignContent: "center",
+                    marginTop: "8%", 
+                    marginLeft: "-3%"
                 }}>
-                <Link to = "/intro">
-                    <Card { ...{title: "Introdução à lógica", img: 'intro-card', txt: "É que nem matemática, só que mais legal!"}} />
-                </Link>
+                
+                <div style = {{width: "105%"}}>
+                    <Link to = "/intro">
+                        <Card { ...{title: "Introdução à lógica", img: 'intro-card', txt: "É que nem matemática, só que mais legal!"}} />
+                    </Link>
+                </div>
 
-                <Link to = "/item/silogismo">
-                    <Card { ...{ title: "Silogismo", img: 'silogismo-card', txt: "Pense como Aristóteles!" } }/>
-                </Link>
+                <div style = {{width: "105%"}}>
+                    <Link to = "/item/silogismo">
+                        <Card { ...{ title: "Silogismo", img: 'silogismo-card', txt: "Pense como Aristóteles!" } }/>
+                    </Link>
+                </div>
 
-                <Link to = "/item/lógica_proposicional">
-                    <Card { ...{ title: "Lógica proposicional", img: "lógica_proposicional-card", txt: "Pense como um computador!" }} />
-                </Link>
+                <div style = {{width: "105%"}}>
+                    <Link to = "/item/lógica_proposicional">
+                        <Card { ...{ title: "Lógica proposicional", img: "lógica_proposicional-card", txt: "Pense como um computador!" }} />
+                    </Link>
+                </div>
+                
             </div>
             
         </main>
